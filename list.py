f58039d8e394d02313451416eac584daf11fda24
Mehran Janfeshan <mehran.janfeshan@joyn.de>
2020-12-24 23:14:47 +0100

    first commit

---
+++ list.py
@@ -0,0 +1,40 @@
+my_list = [1, 2, 3]
+
+# get size of list
+print(len(my_list))
+
+# slicing and indexing work same as array!
+print(my_list[0])
+
+# concatenating two lists
+my_list2 = [4, 5, 6]
+print(my_list + my_list2)
+
+# Update the list items
+my_list[0] = 100
+print(my_list)
+
+# Append an item to the list
+my_list.append(345)
+print(my_list)
+
+# Removing the last item from the list
+print(my_list.pop())
+print(my_list)
+
+# sorting list - This changes the list and it does not return back anything!
+my_list3 = ['a', 'n', 'c', 'f']
+my_list4 = [5, 1, 4, 0, 3]
+my_list3.sort()
+my_list4.sort()
+print(my_list3)
+print(my_list4)
+
+
+# Reverse method - This changes the list and it does not return back anything!
+my_list3.reverse()
+my_list4.reverse()
+print(my_list3)
+print(my_list4)
+
+
---
+++ string.py
@@ -0,0 +1,10 @@
+print("The {} {} {}".format(1, 2, 3))
+
+print("The {2} {1} {0}".format('Merhan', 'Jan', 'shan'))
+
+print("The {f} {b} {c}".format(f='Merhan', b='Jan', c='shan'))
+
+my_name = 'Mehran'
+print("My name is {n}".format(n=my_name))
+
+print(f"My name is {my_name}")
