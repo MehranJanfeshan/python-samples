550c77a46e514c0d80e940f4394d4cbab4eb55b3
Mehran Janfeshan <mehran.janfeshan@joyn.de>
2020-12-25 09:35:22 +0100

    Adding dictionary

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print(my_dict)
+
+# get value for the key
+print(my_dict['key1'])
+
+# dictionary is very flexible and can hold list, or another dictionary
+my_dict2 = {'key1': [1, 3, 4, 5], 'key2': {'key1': 'mehran', 'key2': 'auto'}}
+print(my_dict2['key2']['key1'])
+
+# Adding key value to the an existing dictionary
+my_dict2['key3'] = 123
+print(my_dict2)
+
+# Overwrite a key value
+my_dict2['key3'] = 456
+print(my_dict2)
+
+# get all the keys
+print(my_dict.keys())
+
+# get items in for of Tuples
+print(my_dict.items())
+
+# get values
+print(my_dict.values())
+
+
