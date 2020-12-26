2f799361839ec95cccb64c621155eb3da858f00b
mehranjanfeshan <mehran.janfeshan@joyn.de>
2020-12-26 17:02:15 +0100

    Adding tuples

---
+++ data-structures/tuples.py
@@ -0,0 +1,11 @@
+# tuple is similar to list and the main difference is that items cannot be changed once they are set
+# Also tuple has less builtin functions!
+my_tuple = (1, 2, 3)
+print(type(my_tuple))
+
+# count returns back how many times an item repeated!
+print(my_tuple.count(1))
+
+# index returns back the index of an item first time occurs
+print(my_tuple.index(1))
+
