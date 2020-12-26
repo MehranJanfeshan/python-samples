85429d5bab4d5863c971e94b66314bd1d0502732
mehranjanfeshan <mehran.janfeshan@joyn.de>
2020-12-26 17:07:08 +0100

    Adding sets

---
+++ data-structures/set.py
@@ -0,0 +1,15 @@
+# Sets are unordered collections of unique elements
+
+# Repeated values will be ignored!
+my_set = set()
+my_set.add(1)
+my_set.add(1)
+my_set.add(2)
+print(my_set)
+
+# Convert list to set
+my_list = [1, 1, 1, 1, 1, 3, 3, 3, 3, 6, 6, 6, 6, 2, 2, 2, 7, 7, 7]
+my_set = set(my_list)
+print(my_set)
+
+
