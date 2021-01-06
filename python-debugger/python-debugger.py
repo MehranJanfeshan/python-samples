ecb32c37dc5a691dbe9595e9e84f20de002885c3
mehranjanfeshan <janfeshan.mehran@gmail.com>
2021-01-06 20:56:05 +0100

    Debugger

---
+++ python-debugger/python-debugger.py
@@ -0,0 +1,12 @@
+import pdb
+
+x = [1, 2, 3]
+y = 2
+z = 3
+result_one = y + z
+
+# program stops here any you can explore and get the value of variables to debug.
+# You can stop debugger by typing q and press enter!
+pdb.set_trace()
+
+result_two = y + x
